4f9ef1e5264829d27411a6287e4960ce4bd369f3
hiro246 <roki_stereo@yahoo.co.jp>
2020-08-18 19:30:10 +0900

    Add reverse.js

---
+++ Unit_5/Lesson_23/reverse.js
@@ -0,0 +1,7 @@
+myArray[Symbol.iterator] = function* () {
+    const copy = [ ...this ]; <1>
+    copy.reverse();
+    for (const item of copy) yield item;
+  }
+  const backwards = [ ...myArray ]
+  
