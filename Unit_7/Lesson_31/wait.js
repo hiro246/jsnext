2dfbde8192bab477612415e9bc345149598b8a33
hiro246 <roki_stereo@yahoo.co.jp>
2020-10-22 18:34:13 +0900

    Add wait.js

---
+++ Unit_7/Lesson_31/wait.js
@@ -0,0 +1,10 @@
+function wait(milliseconds) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, milliseconds);
+    });
+  }
+  
+  wait(5000).then(() => {
+    // 5 seconds later...
+  });
+  
