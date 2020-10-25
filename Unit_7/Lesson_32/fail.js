d21027f80e5880a417cae391ed8ef2d11ca8c940
hiro246 <roki_stereo@yahoo.co.jp>
2020-10-26 08:48:57 +0900

    Add fail.js

---
+++ Unit_7/Lesson_32/fail.js
@@ -0,0 +1,19 @@
+async function fail() {
+    const msg = await Promise.reject('I failed.');
+    return 'I Succeeded.';
+  };
+  
+  fail().then(msg => console.log(msg), msg => console.log(msg))
+  
+  async function tryTest() {
+    try {
+      return Promise.reject('err err');
+    } catch(err) {
+      return Promise.resolve(`caught: ${err}`);
+    }
+  }
+  
+  tryTest().then(response => {
+    console.log('response:', response);
+  });
+  
